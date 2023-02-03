f3f46535aff2b9d3786833fadc042e53c68e70de
carly <carlyyiao@gmail.com>
2023-02-03 02:31:11 -0500

    updating repo

--- WeatherPy/Starter_Code/starter_code/api_keys.py
+++ WeatherPy/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4cb7bafc49ed7fb045c532ffa17a2729"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e8e7daa50b914d6f907ee9143ac08fa8"
